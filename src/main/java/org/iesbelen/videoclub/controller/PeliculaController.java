803df258ab2babc4127d84f1329794d5fd1de1cf
Frigosamu <Frigosamu@gmail.com>
2025-03-05 19:49:51 +0100

    arreglo final 2

--- src/main/java/org/iesbelen/videoclub/controller/PeliculaController.java
+++ src/main/java/org/iesbelen/videoclub/controller/PeliculaController.java
@@ -27,27 +27,63 @@ public class PeliculaController {
     @Autowired
     private CategoriaService categoriaService;
 
-    @Autowired
-    private PeliculaRepository peliculaRepository;
-
     public PeliculaController(PeliculaService peliculaService) {
         this.peliculaService = peliculaService;
     }
 
-    @GetMapping(value = {"", "/"}, params = {"!buscar", "!ordenar", "!pagina", "!tamanio"})
+    @GetMapping(value = {"","/"}, params = {"!buscar", "!ordenar", "!paginado", "!orden"})
     public List<Pelicula> all() {
         log.info("Accediendo a todas las películas");
         return this.peliculaService.all();
     }
 
+    @GetMapping(value = {"/order"})
+    public List<Pelicula> allOrdered() {
+        log.info("Accediendo a todas las películas ordenadas");
+        return this.peliculaService.findAllByOrderByTituloAsc();
+    }
+
+    @GetMapping(value = {"","/"}, params = {"!paginado", "!orden"})
+    public List<Pelicula> all(@RequestParam("buscar") Optional<String> buscarOptional,
+                              @RequestParam("ordenar") Optional<String> ordenarOptional) {
+
+        log.info("Accediendo a todas las películas con filtro buscar: %s y ordenar: %s",
+                buscarOptional.orElse(""),
+                ordenarOptional.orElse(""));
+        return this.peliculaService.findAllByQueryFilter(buscarOptional, ordenarOptional);
+    }
+
+    @GetMapping(value = {"","/"}, params = {"!buscar", "!ordenar", "!paginado"})
+    public ResponseEntity<List<Pelicula>> allbyColumn(@RequestParam(value = "orden", required = false) String[] orden) {
+        log.info("Accediendo a todas las películas con ordenación por columna: " + orden[0]);
+
+        List<Pelicula> peliculas = this.peliculaService.findAllOrderByCol(orden);
+
+        return ResponseEntity.ok(peliculas);
+    }
+
+    // Para ordenar por dos columnas
+    /*@GetMapping(value = {"","/"}, params = {"!buscar", "!ordenar", "!paginado"})
+    public ResponseEntity<List<Pelicula>> allbyColumn(@RequestParam(value = "orden", required = false) String[] orden) {
+        log.info("Accediendo a todas las películas con ordenación por columnas: " + orden[0]);
+
+        List<Pelicula> peliculas = this.peliculaService.findAllOrderByCols(orden);
+
+        return ResponseEntity.ok(peliculas);
+    }*/
+
+    @GetMapping(value = {"","/"}, params = {"!buscar", "!ordenar", "!orden"})
+    public ResponseEntity<Map<String, Object>> all(@RequestParam(value = "paginado", defaultValue = "0") String[] paginacion) {
+        log.info("Accediendo a todas las películas con paginación");
+
+        Map<String, Object> responseAll = this.peliculaService.all(paginacion);
+
+        return ResponseEntity.ok(responseAll);
+    }
+
     @PostMapping({"","/"})
     public Pelicula newPelicula(@RequestBody Pelicula pelicula) {
         return this.peliculaService.save(pelicula);
-    }
-
-    @PostMapping("/{id}/add/{id_categoria}")
-    public void addPelicula(@PathVariable long id, @PathVariable long id_categoria) {
-        this.peliculaService.addCategoria(id, id_categoria);
     }
 
     @GetMapping("/{id}")
@@ -67,37 +103,8 @@ public class PeliculaController {
         this.peliculaService.delete(id);
     }
 
-    @GetMapping(value = {"/order"})
-    public List<Pelicula> allOrdanado() {
-        log.info("Accediendo a todas las películas ordenadas");
-        return this.peliculaService.findAllByOrderByTituloAsc();
-    }
-
-    @GetMapping(value = {"","/"}, params = {"!pagina", "!tamanio"})
-    public List<Pelicula> all(@RequestParam("buscar") Optional<String> buscarOptional, @RequestParam("ordenar") Optional<String> ordenarOptional) {
-        log.info("Accediendo a todas las películas con filtro buscar: %s y ordenadr: %s",
-                buscarOptional.orElse(""),
-                ordenarOptional.orElse(""));
-
-        return this.peliculaService.findAllByQueryFilter(buscarOptional, ordenarOptional);
-    }
-
-    @GetMapping(value = {"" , "/"}, params = {"!buscar", "!ordenar"})
-    public ResponseEntity<Map<String, Object>> all(@RequestParam(value = "pagina", defaultValue = "0") int pagina,
-                                                   @RequestParam(value = "tamanio", defaultValue = "3") int tamanio) {
-        log.info("Accediendo a todas las películas con paginación");
-
-        Map<String, Object> responseAll = this.peliculaService.all(pagina, tamanio);
-
-        return ResponseEntity.ok(responseAll);
-    }
-
-    @GetMapping(value = {"", "/"}, params = {"orden"})
-    public ResponseEntity<List<Pelicula>> obtenerPeliculas(
-            @RequestParam(required = false) String[] orden) {
-
-        // Si no se pasa ningún parámetro 'orden', el valor será null
-        List<Pelicula> peliculas = peliculaService.obtenerPeliculaConOrdenYPaginado(orden);
-        return ResponseEntity.ok(peliculas);
+    @PostMapping("/{id}/addCategoria/{id_categoria}")
+    public Pelicula addCategoria(@PathVariable("id") Long id, @PathVariable("id_categoria") Long idCategoria) {
+        return this.peliculaService.addCategoria(id, idCategoria);
     }
 }
